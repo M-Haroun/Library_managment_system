1af53ad2f65f21b59e3ca0d90bb8f12935ff411c
M_Haroun <mahmoudharoun2@gmail.com>
2024-03-31 00:32:00 +0200

    add pages and books apps in setting

--- Library_management_system/settings.py
+++ Library_management_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.Pages.Config',
+    'books.apps.Books.Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +55,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'Library_management_system.urls'
 
 TEMPLATES = [
+    os.path.join(BASE_DIR,'templates'),
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
