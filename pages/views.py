b77f06a6d02b05a0755d7598d88941a2a706870d
M_Haroun <mahmoudharoun2@gmail.com>
2024-04-28 00:49:53 +0200

    Add categories to side bar

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from books.models import Book
+from books.models import Book, Category
 # Create your views here.
 
 def index(request):
-    data=Book.objects.all()
-    return render(request,'pages/index.html',{'books':data})
+    Book_data=Book.objects.all()
+    Cat_data=Category.objects.all()
+    return render(request,'pages/index.html',{'books':Book_data,'categories':Cat_data})
