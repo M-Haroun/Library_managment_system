2503a1c2d6c670f92f3ffb0b01ba8fe8297e86db
M_Haroun <mahmoudharoun2@gmail.com>
2024-05-06 02:01:32 +0200

    make the 4 categories that shown in index page are shown randomly

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from books.models import Book, Category
+from random import sample
+
 # Create your views here.
 
 def index(request):
     Book_data=Book.objects.all()
     Cat_data=Category.objects.all()
-    return render(request,'pages/index.html',{'books':Book_data,'categories':Cat_data})
+    Cat_random = sample(list(Cat_data), 4)
+    return render(request,'pages/index.html',{'books':Book_data,'categories':Cat_data,
+                                              'random_categories':Cat_random})
