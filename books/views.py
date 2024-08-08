f71568ff5a15a9a944e1a9104e881e984d4362bc
M_Haroun <mahmoudharoun2@gmail.com>
2024-08-08 13:09:12 +0200

    handeling search by prices, books titles and authers names

--- books/views.py
+++ books/views.py
@@ -1,20 +1,38 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from .models import Book, Category
-from .forms import CategoryForm, BookForm
+from .forms import CategoryForm
 from pages.views import update,delete
+from django.db.models import Q
 
 # Create your views here.
 def books(request):
-    Book_data=Book.objects.all()
+    search=Book.objects.all()
     Cat_data=Category.objects.all()
     Category_form=CategoryForm()
+    input_value = None
+    
+    if 'search_name' in request.GET:
+        input_value = request.GET['search_name']
+        if input_value:
+            # Check the input values are the numeric or string
+            # if numeric search in prices else serch in title or auther names
+            try:
+                price = float(input_value)
+                search = search.filter(
+                    Q(price__lte = price)|Q(retal_price_day__lte = price))
+            except ValueError:
+                # Use Q objects to combine filters
+                search = search.filter( 
+                                   Q(title__icontains = input_value) | Q(author__icontains = input_value)
+                                   )
+    
     
     if request.method == 'POST':      
         Category_data = CategoryForm(request.POST)
         if Category_data.is_valid():
             Category_data.save()
              
-    context = {'books':Book_data,
+    context = {'books':search,
                'categories':Cat_data,
                'categoryform':Category_form,
                }
